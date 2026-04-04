ad5c44da97421d6a01385988120983ab96b3de6d
Fuyu <31686802+winteeeryan@users.noreply.github.com>
2026-04-03 22:58:26 -0700

    Update intro-overlay.tsx

--- components/intro-overlay.tsx
+++ components/intro-overlay.tsx
@@ -1,1 +1,31 @@
+"use client";
 
+import { useState } from "react";
+import dynamic from "next/dynamic";
+
+const P5IntroSketch = dynamic(() => import("./p5-intro-sketch"), {
+  ssr: false,
+});
+
+type Props = {
+  onFinish: () => void;
+};
+
+export default function IntroOverlay({ onFinish }: Props) {
+  const [isExiting, setIsExiting] = useState(false);
+
+  const handleFinish = () => {
+    setIsExiting(true);
+    window.setTimeout(() => {
+      onFinish();
+    }, 800);
+  };
+
+  return (
+    <div className={`intro-overlay ${isExiting ? "is-exiting" : ""}`}>
+      <div className="intro-stage">
+        <P5IntroSketch onFinish={handleFinish} />
+      </div>
+    </div>
+  );
+}
