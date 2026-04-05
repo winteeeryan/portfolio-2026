7117c6a050e24fab5c44be36665fdd2b9d954e07
Fuyu <31686802+winteeeryan@users.noreply.github.com>
2026-04-05 13:34:28 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import IntroGate from "@/components/intro/intro-gate";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <IntroGate />
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
